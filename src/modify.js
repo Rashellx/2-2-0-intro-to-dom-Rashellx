9f951d62b89fbaaeb9f7ee6f93fa184552e17292
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-05-15 09:55:26 -0400

    work

--- src/modify.js
+++ src/modify.js
@@ -1,22 +1,47 @@
 const getMainHeadingText = () => {
+  //document.querySelector("#main-heading")
+  console.log(document.querySelector("#main-heading").textContent)
 };
 
 const getAllMainText = () => {
+  const main = document.querySelectorAll(".main-text")
+  let newText = ""
+  main.forEach((main) => newText += main.textContent + ",")
+  console.log(newText.slice(0, -1))
 };
 
 const setSubtitleText = () => {
+  document.querySelector("h2").textContent = "This is a subtitle!"
 };
 
 const modifyDivClassList = () => {
+  const list = document.querySelector("#modify-classes").classList
+  list.remove("bad-class")
+  list.add("new-class")
 };
-
+//Question 5
 const addH2 = () => {
-};
+  const newElement = document.createElement("h2")
+  newElement.id = "h2-test"
+  newElement.textContent = "Another one!"
+  document.body.appendChild(newElement)
+}
 
 const removeOldInfo = () => {
+  let element = document.querySelector("#old-info")
+  element.remove()
 };
 
 const makeAlphabet = () => {
+  const numOfLetters = document.querySelector("#alphabet").dataset.numLetters
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const letters = alphabet.split(" ")
+  for (let i = 0; i < numOfLetters; i++) {
+    const newlist = document.createElement("li")
+    newlist.textContent = ` ${letters[i]} is letter #${i + 1} in the alphabet`
+    hi
+  }
+
 };
 
 const makeBio = () => {
